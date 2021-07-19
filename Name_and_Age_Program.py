a8270897ebc3c4983fcc772cdc13db05d4deafb8
nikhilreddypanyam <87633457+nikhilreddypanyam@users.noreply.github.com>
2021-07-19 22:21:41 +0530

    Add files via upload

---
+++ Name_and_Age_Program.py
@@ -0,0 +1,10 @@
+Name = str(input("Enter you Name: "))
+age = int(input("Enter you Age: "))
+num = int(input("number of time to print the 100th year : "))
+n = (2021 - age)
+year = (n + (100))
+print("Your 100th year is ",year)
+
+x = range(num)
+for i in x: 
+	print(year)	
